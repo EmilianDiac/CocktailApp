682810bde04abd45a307de77b7b0650a45aeba2e
EmilianDiac <diac.emilian@gmail.com>
2020-10-23 00:34:06 +0300

    Added cach-ing strategy for fetching the data.

--- app/src/main/java/com/example/android/cocktailapp/database/CocktailDAO.kt
+++ app/src/main/java/com/example/android/cocktailapp/database/CocktailDAO.kt
@@ -1,7 +1,6 @@
 package com.example.android.cocktailapp.database
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -10,10 +9,13 @@ import androidx.room.Query
 @Dao
 interface FavoritesDAO{
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(cocktail: FavoriteCocktail)
+    suspend fun insert(cocktail: DatabaseCocktail)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAll(cocktailList: List<DatabaseCocktail>)
 
     @Query("SELECT * FROM favorite_cocktails_table ORDER BY name ASC")
-    fun getAllFavoriteCocktails(): LiveData<MutableList<FavoriteCocktail>>
+    fun getAllFavoriteCocktails(): LiveData<MutableList<DatabaseCocktail>>
 
     @Query("DELETE FROM favorite_cocktails_table")
     suspend fun deleteAllFavoriteCocktails()
---
+++ app/src/main/java/com/example/android/cocktailapp/database/DatabaseEntities.kt
@@ -0,0 +1,26 @@
+package com.example.android.cocktailapp.database
+
+import android.provider.ContactsContract
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.cocktailapp.domain.DomainCocktail
+
+@Entity(tableName = "favorite_cocktails_table")
+data class DatabaseCocktail(
+    val name: String,
+    @ColumnInfo(name = "image_url") val imageUrl: String,
+    @PrimaryKey @ColumnInfo(name = "cocktail_id") val cocktailId: Long,
+    @ColumnInfo(name = "is_favorite") val isFavorite: Boolean
+)
+
+fun List<DatabaseCocktail>.asDomainCocktail(): List<DomainCocktail> {
+    return map {
+        DomainCocktail(
+            name = it.name,
+            id = it.cocktailId,
+            imageUrl = it.imageUrl,
+            isFavorite = it.isFavorite
+        )
+    }
+}
