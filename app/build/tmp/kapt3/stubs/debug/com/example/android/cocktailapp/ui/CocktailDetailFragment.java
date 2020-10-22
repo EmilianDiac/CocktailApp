b8f3b627da505bd389a58926c8a06a65b55521b3
EmilianDiac <diac.emilian@gmail.com>
2020-10-22 20:06:56 +0300

    Updated the gitignore file

--- app/build/tmp/kapt3/stubs/debug/com/example/android/cocktailapp/ui/CocktailDetailFragment.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/cocktailapp/ui/CocktailDetailFragment.java
@@ -2,26 +2,16 @@ package com.example.android.cocktailapp.ui;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000N\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0011\u001a\u00020\u00122\b\u0010\u0013\u001a\u0004\u0018\u00010\u0014H\u0016J&\u0010\u0015\u001a\u0004\u0018\u00010\u00162\u0006\u0010\u0017\u001a\u00020\u00182\b\u0010\u0019\u001a\u0004\u0018\u00010\u001a2\b\u0010\u0013\u001a\u0004\u0018\u00010\u0014H\u0016J\u001a\u0010\u001b\u001a\u00020\u00122\u0006\u0010\u001c\u001a\u00020\u00162\b\u0010\u0013\u001a\u0004\u0018\u00010\u0014H\u0016J\u0018\u0010\u001d\u001a\u00020\u00122\u0006\u0010\u001e\u001a\u00020\u001f2\u0006\u0010 \u001a\u00020!H\u0002R\u001b\u0010\u0003\u001a\u00020\u00048FX\u0086\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0007\u0010\b\u001a\u0004\b\u0005\u0010\u0006R\u000e\u0010\t\u001a\u00020\nX\u0082.\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u000b\u001a\u00020\f8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u000f\u0010\u0010\u001a\u0004\b\r\u0010\u000e\u00a8\u0006\""}, d2 = {"Lcom/example/android/cocktailapp/ui/CocktailDetailFragment;", "Landroidx/fragment/app/Fragment;", "()V", "args", "Lcom/example/android/cocktailapp/ui/CocktailDetailFragmentArgs;", "getArgs", "()Lcom/example/android/cocktailapp/ui/CocktailDetailFragmentArgs;", "args$delegate", "Landroidx/navigation/NavArgsLazy;", "cocktailDetailViewModel", "Lcom/example/android/cocktailapp/viewModels/CocktailDetailViewModel;", "sharedViewModel", "Lcom/example/android/cocktailapp/viewModels/MainActivityViewModel;", "getSharedViewModel", "()Lcom/example/android/cocktailapp/viewModels/MainActivityViewModel;", "sharedViewModel$delegate", "Lkotlin/Lazy;", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "onViewCreated", "view", "updateUI", "binding", "Lcom/example/android/cocktailapp/databinding/FragmentCocktailDetailBinding;", "details", "Lcom/example/android/cocktailapp/api/CocktailDetails;", "app_debug"})
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000F\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J&\u0010\u000b\u001a\u0004\u0018\u00010\f2\u0006\u0010\r\u001a\u00020\u000e2\b\u0010\u000f\u001a\u0004\u0018\u00010\u00102\b\u0010\u0011\u001a\u0004\u0018\u00010\u0012H\u0016J\u001a\u0010\u0013\u001a\u00020\u00142\u0006\u0010\u0015\u001a\u00020\f2\b\u0010\u0011\u001a\u0004\u0018\u00010\u0012H\u0016J\u0018\u0010\u0016\u001a\u00020\u00142\u0006\u0010\u0017\u001a\u00020\u00182\u0006\u0010\u0019\u001a\u00020\u001aH\u0002R\u001b\u0010\u0003\u001a\u00020\u00048FX\u0086\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0007\u0010\b\u001a\u0004\b\u0005\u0010\u0006R\u000e\u0010\t\u001a\u00020\nX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001b"}, d2 = {"Lcom/example/android/cocktailapp/ui/CocktailDetailFragment;", "Landroidx/fragment/app/Fragment;", "()V", "args", "Lcom/example/android/cocktailapp/ui/CocktailDetailFragmentArgs;", "getArgs", "()Lcom/example/android/cocktailapp/ui/CocktailDetailFragmentArgs;", "args$delegate", "Landroidx/navigation/NavArgsLazy;", "cocktailDetailViewModel", "Lcom/example/android/cocktailapp/viewModels/CocktailDetailViewModel;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onViewCreated", "", "view", "updateUI", "binding", "Lcom/example/android/cocktailapp/databinding/FragmentCocktailDetailBinding;", "details", "Lcom/example/android/cocktailapp/api/CocktailDetails;", "app_debug"})
 public final class CocktailDetailFragment extends androidx.fragment.app.Fragment {
     @org.jetbrains.annotations.NotNull()
     private final androidx.navigation.NavArgsLazy args$delegate = null;
-    private final kotlin.Lazy sharedViewModel$delegate = null;
     private com.example.android.cocktailapp.viewModels.CocktailDetailViewModel cocktailDetailViewModel;
     private java.util.HashMap _$_findViewCache;
     
     @org.jetbrains.annotations.NotNull()
     public final com.example.android.cocktailapp.ui.CocktailDetailFragmentArgs getArgs() {
         return null;
-    }
-    
-    private final com.example.android.cocktailapp.viewModels.MainActivityViewModel getSharedViewModel() {
-        return null;
-    }
-    
-    @java.lang.Override()
-    public void onCreate(@org.jetbrains.annotations.Nullable()
-    android.os.Bundle savedInstanceState) {
     }
     
     @org.jetbrains.annotations.Nullable()
